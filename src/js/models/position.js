6769599d481052658cbeafba0446f528f3e146d5
Luis Preza <preza.luis@gmail.com>
2020-02-14 14:30:09 -0600

    Ship and position models

---
+++ src/js/models/position.js
@@ -0,0 +1,8 @@
+class Position {
+  constructor(id) {
+    this.id = id;
+    this.x= id % 10;
+    this.y= Math.floor(id / 10);
+    this.hited = false;
+  }
+}
---
+++ src/js/models/ship.js
@@ -0,0 +1,24 @@
+class Ship {
+  constructor() {
+    this.positions = [];
+  }
+  get length() {
+    return this.positions.length;
+  }
+  hit(id){
+    this.positions.forEach((item) => {
+      if(item.id === id) item.hited = true;
+    });
+  }
+  hitXY(x,y){
+    this.positions.forEach((item) => {
+      if(item.x === x && item.y === y) item.hited = true;
+    });
+  }
+  isSunk(){
+    return this.positions.every(x => x.hited);
+  }
+  addPosition(position){
+    this.positions.push(position);
+  }
+}
